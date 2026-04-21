20541d417eb89bd94a0b85c74f52eb2a22139c48
Daniela Maldonado <dmaldonado@fi.uba.ar>
2026-04-21 09:42:29 -0300

    implementa iterador externo

--- lista.go
+++ lista.go
@@ -1,1 +1,9 @@
 package lista
+
+type IteradorLista[T any] interface {
+	VerActual() T
+	HayAlgoMas() bool
+	Avanzar()
+	Insertar(T)
+	Borrar() T
+}
--- lista_enlazada.go
+++ lista_enlazada.go
@@ -1,1 +1,54 @@
 package lista
+
+type nodoLista[T any] struct {
+	dato      T
+	siguiente *nodoLista[T]
+}
+
+type listaEnlazada[T any] struct {
+	primero *nodoLista[T]
+	ultimo  *nodoLista[T]
+	largo   int
+}
+
+type iteradorLista[T any] struct {
+	actual   *nodoLista[T]
+	anterior *nodoLista[T]
+	lista    *listaEnlazada[T]
+}
+
+func (lista *listaEnlazada[T]) Iterador() IteradorLista[T] {
+	return &iteradorLista[T]{
+		actual:   lista.primero,
+		anterior: nil,
+		lista:    lista,
+	}
+}
+
+func (iter *iteradorLista[T]) HayAlgoMas() bool {
+	return iter.actual != nil
+}
+
+func (iter *iteradorLista[T]) VerActual() T {
+	if iter.actual == nil {
+		panic("El iterador termino de iterar")
+	}
+	return iter.actual.dato
+}
+
+func (iter *iteradorLista[T]) Avanzar() {
+	if iter.actual == nil {
+		panic("El iterador termino de iterar")
+	}
+	iter.anterior = iter.actual
+	iter.actual = iter.actual.siguiente
+}
+
+func (iter *iteradorLista[T]) Insertar(valor T) {
+	nuevo := &nodoLista[T]{dato: valor}
+
+	if iter.anterior == nil {
+		nuevo.siguiente = iter.lista.primero
+	}
+
+}
