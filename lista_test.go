b62cb33ca767b39cab1792a9f87e88d7d96fe0ba
ElMoche <pperezm@fi.uba.ar>
2026-04-22 01:57:57 -0300

    ahora si, deberian de estar todas las pruebas de lista enlazada, solo faltarian la de los 2 iteradores, cualquier cosa que veas mal dani cambiala

--- lista_test.go
+++ lista_test.go
@@ -34,7 +34,7 @@ func TestInsertarPrimero(t *testing.T) {
 	require.False(t, lista_strings.EstaVacia())
 
 	lista_float64.InsertarPrimero(10.2)
-	require.False(t, lista_enteros.EstaVacia())
+	require.False(t, lista_float64.EstaVacia())
 }
 
 func TestInsertarUltimo(t *testing.T) {
@@ -126,6 +126,7 @@ func TestBorrarPrimero(t *testing.T) {
 
 	lista_enteros.InsertarPrimero(10)
 	lista_enteros.BorrarPrimero()
+
 	require.True(t, lista_enteros.EstaVacia())
 
 	lista_strings.InsertarPrimero("martu")
@@ -135,6 +136,42 @@ func TestBorrarPrimero(t *testing.T) {
 	lista_float64.InsertarPrimero(10.2)
 	lista_float64.BorrarPrimero()
 	require.True(t, lista_float64.EstaVacia())
+}
+
+func TestBorrarPrimeroDevuelveElPrimerElemento(t *testing.T) {
+	lista_enteros, lista_strings, lista_float64 := crearListasParaTestear()
+
+	lista_enteros.InsertarPrimero(10)
+	lista_enteros.InsertarUltimo(15)
+	require.Equal(t, 10, lista_enteros.BorrarPrimero())
+
+	lista_strings.InsertarPrimero("martu")
+	lista_strings.InsertarPrimero("dani")
+	require.Equal(t, "dani", lista_strings.BorrarPrimero())
+
+	lista_float64.InsertarPrimero(10.2)
+	lista_float64.InsertarPrimero(44.3)
+	require.Equal(t, 44.3, lista_strings.BorrarPrimero())
+
+}
+
+func TestBorrarPrimeroEnUnaListaVacia(t *testing.T) {
+	lista_enteros, lista_strings, lista_float64 := crearListasParaTestear()
+
+	lista_enteros.InsertarPrimero(10)
+	lista_enteros.BorrarPrimero()
+	require.True(t, lista_enteros.EstaVacia())
+	require.Panics(t, func() { lista_enteros.BorrarPrimero() })
+
+	lista_strings.InsertarPrimero("tengosueño")
+	lista_strings.BorrarPrimero()
+	require.True(t, lista_strings.EstaVacia())
+	require.Panics(t, func() { lista_strings.BorrarPrimero() })
+
+	lista_float64.InsertarPrimero(10.2)
+	lista_float64.BorrarPrimero()
+	require.True(t, lista_float64.EstaVacia())
+	require.Panics(t, func() { lista_float64.BorrarPrimero() })
 }
 
 func TestBorrarPrimeroConvierteAlSegundoEnPrimero(t *testing.T) {
@@ -159,23 +196,104 @@ func TestBorrarPrimeroConvierteAlSegundoEnPrimero(t *testing.T) {
 	require.Equal(t, 1.2343, lista_float64.VerPrimero())
 }
 
-func TestLargo(t *testing.T) {
+func TestLargoIncremento(t *testing.T) {
 	lista_enteros, lista_strings, lista_float64 := crearListasParaTestear()
 
 	lista_enteros.InsertarPrimero(10)
+	require.Equal(t, 1, lista_enteros.Largo())
 	lista_enteros.InsertarUltimo(100)
+	require.Equal(t, 2, lista_enteros.Largo())
 	lista_enteros.InsertarUltimo(10000)
 	require.Equal(t, 3, lista_enteros.Largo())
 
 	lista_strings.InsertarPrimero("coscu")
+	require.Equal(t, 1, lista_strings.Largo())
 	lista_strings.InsertarUltimo("nachoide")
+	require.Equal(t, 2, lista_strings.Largo())
 	lista_strings.InsertarUltimo("nanoide")
 	require.Equal(t, 3, lista_strings.Largo())
 
 	lista_float64.InsertarPrimero(1.23)
+	require.Equal(t, 1, lista_float64.Largo())
 	lista_float64.InsertarUltimo(1.2343)
+	require.Equal(t, 2, lista_float64.Largo())
 	lista_float64.InsertarUltimo(2.34)
 	require.Equal(t, 3, lista_float64.Largo())
+}
+
+func TestLargoDecrecimiento(t *testing.T) {
+	lista_enteros, lista_strings, lista_float64 := crearListasParaTestear()
+
+	lista_enteros.InsertarPrimero(40)
+	lista_enteros.InsertarPrimero(67)
+	lista_enteros.InsertarPrimero(5)
+	require.Equal(t, 3, lista_enteros.Largo())
+	lista_enteros.BorrarPrimero()
+	require.Equal(t, 2, lista_enteros.Largo())
+	lista_enteros.BorrarPrimero()
+	require.Equal(t, 1, lista_enteros.Largo())
+	lista_enteros.BorrarPrimero()
+	require.Equal(t, 0, lista_enteros.Largo())
+
+	lista_strings.InsertarPrimero("gradiente")
+	lista_strings.InsertarPrimero("mli")
+	lista_strings.InsertarPrimero("proyecto")
+	require.Equal(t, 3, lista_strings.Largo())
+	lista_strings.BorrarPrimero()
+	require.Equal(t, 2, lista_strings.Largo())
+	lista_strings.BorrarPrimero()
+	require.Equal(t, 1, lista_strings.Largo())
+	lista_strings.BorrarPrimero()
+	require.Equal(t, 0, lista_strings.Largo())
+
+	lista_float64.InsertarPrimero(40.2)
+	lista_float64.InsertarPrimero(67.67)
+	lista_float64.InsertarPrimero(5.3)
+	require.Equal(t, 3, lista_float64.Largo())
+	lista_float64.BorrarPrimero()
+	require.Equal(t, 2, lista_float64.Largo())
+	lista_float64.BorrarPrimero()
+	require.Equal(t, 1, lista_float64.Largo())
+	lista_float64.BorrarPrimero()
+	require.Equal(t, 0, lista_float64.Largo())
+
+}
+
+func TestPrimeroYUltimoSonElMismoElementoEnUnaListaDeLargoUno(t *testing.T) {
+	lista_enteros, lista_strings, lista_float64 := crearListasParaTestear()
+
+	lista_enteros.InsertarPrimero(10)
+	require.Equal(t, lista_enteros.VerPrimero(), lista_enteros.VerUltimo())
+
+	lista_strings.InsertarPrimero("jordiwild")
+	require.Equal(t, lista_strings.VerPrimero(), lista_strings.VerUltimo())
+
+	lista_float64.InsertarPrimero(10.89)
+	require.Equal(t, lista_float64.VerPrimero(), lista_float64.VerUltimo())
+
+}
+
+func TestExtremosVerPrimeroYVerUltimo(t *testing.T) {
+	lista_enteros, lista_strings, lista_float64 := crearListasParaTestear()
+
+	lista_enteros.InsertarPrimero(10)
+	lista_enteros.InsertarPrimero(15)
+	lista_enteros.InsertarUltimo(55)
+	require.Equal(t, 15, lista_enteros.VerPrimero())
+	require.Equal(t, 55, lista_enteros.VerUltimo())
+
+	lista_strings.InsertarPrimero("+")
+	lista_strings.InsertarPrimero("{}")
+	lista_strings.InsertarUltimo("*")
+	require.Equal(t, "{}", lista_strings.VerPrimero())
+	require.Equal(t, "*", lista_strings.VerUltimo())
+
+	lista_float64.InsertarPrimero(10.44)
+	lista_float64.InsertarPrimero(15.44)
+	lista_float64.InsertarUltimo(55.44)
+	require.Equal(t, 15.44, lista_float64.VerPrimero())
+	require.Equal(t, 55.44, lista_float64.VerUltimo())
+
 }
 
 func TestVolumen(t *testing.T) {
